753efa3252bf07e15e32fb1d654400b52c6b46db
Rishabh Gautam <157235362+rishabhgautam12@users.noreply.github.com>
2025-04-11 10:38:42 +0530

    Update App.jsx
    navbar name change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
