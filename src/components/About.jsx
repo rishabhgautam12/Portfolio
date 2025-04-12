f509fa09bfd04c0ec9d02f4ea05d8602887ca47a
Rishabh Gautam <157235362+rishabhgautam12@users.noreply.github.com>
2025-04-12 13:29:09 +0530

    Update About.jsx
    about page changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ const About = () => {
       className="w-full min-h-screen bg-gradient-to-b from-black to-gray-900 text-white pt-20 flex items-center"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full">
-        
         {/* Section Title */}
         <div className="pb-8">
           <h2 className="text-4xl font-bold inline border-b-4 border-gray-500">
@@ -17,23 +16,25 @@ const About = () => {
 
         {/* About Content */}
         <p className="text-xl mt-10">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
+          Hi! I'm Rishabh Gautam, a passionate and curious Software Engineer and
+          MERN Stack Developer. I recently completed my B.Tech in Computer
+          Science and Engineering, and I love building scalable and efficient
+          web applications that solve real-world problems.
         </p>
 
         <br />
 
         <p className="text-xl">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum
-          pariatur, vel similique sint, nobis aspernatur ut praesentium
-          explicabo ipsam aliquid quasi laboriosam et culpa possimus repudiandae
-          quisquam ullam maiores ab unde. Fugiat odio mollitia nemo alias.
-          Commodi facilis atque nulla vero voluptatem explicabo. Quibusdam,
-          magni quo! Eum cupiditate debitis labore.
+          I'm proficient in JavaScript, C++, React with hands-on experience in
+          technologies like Node.js, Express.js, and MongoDB.
+          <br />
+          Some of my favorite projects include an E-commerce platform, an
+          internship automation bot, and my latest work—Attendo, a smart
+          attendance management system with live updates and QR-based check-ins.
+          <br /><br/>
+          I’m currently looking for opportunities where I can grow as a
+          developer, work on impactful projects, and collaborate with
+          like-minded teams.
         </p>
       </div>
     </section>
